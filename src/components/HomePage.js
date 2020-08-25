66550589116f7d7952635363adaada77469ae4b1
Parth Patel <parth_patel@comcast.com>
2020-08-24 21:47:21 -0400

    add first react component

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div className="jumbotron">
+            <h1>Pluralsight, Administration</h1>
+            <p>React, fulx and react router for ultra-responsive web apps.</p>
+        </div>
+    );
+}
+
+export default HomePage;
