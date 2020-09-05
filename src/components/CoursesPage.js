79c440964d0c17d21638c2fd091b77d2c5303c0d
Parth Patel <parth_patel@comcast.com>
2020-09-05 17:39:40 -0400

    call course api and paint course component

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { getCourses } from "../api/courseApi";
 
-function CoursesPage() {
-    return <h2> Courses </h2>;
+class CoursesPage extends React.Component {
+    state = {
+        courses: []
+    }
+
+    componentDidMount() {
+        getCourses().then(courses => this.setState({ courses }));
+    }
+
+    render() {
+        return <>
+        <h2> Courses </h2>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Author ID</th>
+                    <th>Category</th>
+                </tr>
+            </thead>
+            <tbody>
+                { this.state.courses.map((courses) => {
+                    return <tr>
+                        <td>{ courses.title }</td>
+                        <td>{ courses.authorId }</td>
+                        <td>{ courses.category }</td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
+        </>
+    }
+
+    
 }
 
 export default CoursesPage;
