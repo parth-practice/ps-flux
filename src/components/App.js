5ca1754c3930fb04b2ad918f767aeb86a63d1c42
Parth Patel <parth_patel@comcast.com>
2020-09-06 11:09:21 -0400

    use of react browser router

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import AboutPage from './AboutPage';
-import HomePage from './HomePage';
-import Header from './common/Header';
-import CoursesPage from './CoursesPage';
+import React from "react";
+import { Route } from "react-router-dom";
+
+import AboutPage from "./AboutPage";
+import HomePage from "./HomePage";
+import Header from "./common/Header";
+import CoursesPage from "./CoursesPage";
 
 function App() {
-    function getPage() {
-        const route = window.location.pathname;
-
-        if (route === "/about") return <AboutPage />;
-        if (route === "/courses") return <CoursesPage />;
-        return <HomePage />;
-    }
-    return (
-        <div className="container-fluid">
-            <Header />
-            { getPage() }
-        </div>
-    );
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Route path="/" exact component={HomePage}></Route>
+      <Route path="/About" component={AboutPage}></Route>
+      <Route path="/Courses" component={CoursesPage}></Route>
+    </div>
+  );
 }
 
 export default App;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 function HomePage() {
-    return (
-        <div className="jumbotron">
-            <h1>Pluralsight, Administration</h1>
-            <p>React, fulx and react router for ultra-responsive web apps.</p>
-            <a href="/about">About</a>
-        </div>
-    );
+  return (
+    <div className="jumbotron">
+      <h1>Administration</h1>
+      <p>React, fulx and react router for ultra-responsive web apps.</p>
+      <a href="/about">About</a>
+    </div>
+  );
 }
 
 export default HomePage;
