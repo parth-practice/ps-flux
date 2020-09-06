03a981c19b127ba2e09e249fe8b49a9b8f4876c3
Parth Patel <parth_patel@comcast.com>
2020-09-06 10:48:31 -0400

    add proptypes

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function CourseList(props) {
   return (
@@ -24,5 +25,17 @@ function CourseList(props) {
     </table>
   );
 }
+
+CourseList.prototype = {
+  courses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      authorId: PropTypes.number.isRequired,
+      category: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  tableHeaders: PropTypes.object.isRequired,
+};
 
 export default CourseList;
