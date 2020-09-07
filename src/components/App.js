6973202c0596995df82488db7b52822bdb3adf80
Parth Patel <parth_patel@comcast.com>
2020-09-06 20:39:11 -0400

    adding 404 component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import AboutPage from "./AboutPage";
 import HomePage from "./HomePage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
+import NotFoundPage from "./NotFoundPage";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Route path="/" exact component={HomePage}></Route>
-      <Route path="/About" component={AboutPage}></Route>
-      <Route path="/Courses" component={CoursesPage}></Route>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/About" component={AboutPage} />
+        <Route path="/Courses" component={CoursesPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
